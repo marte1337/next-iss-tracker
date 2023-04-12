0bebc209b3c7d84edf69324fe46510a26737829c
Marte Auer <marte_auer@hotmail.com>
2023-04-12 12:35:21 +0200

    add refresh button function

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 import useSWR from "swr";
@@ -19,7 +18,6 @@ const fetcher = async (url) => {
     error.status = res.status;
     throw error;
   }
-
   return res.json();
 };
 
@@ -28,7 +26,8 @@ const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 //
 export default function ISSTracker() {
-  const { data, error, isLoading } = useSWR(URL, fetcher, {
+  //useSWR-hook with error-handling, refreshInterval and mutate-function to fetch data programmatically (here: clicking the "Refresh" button)
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
     refreshInterval: 5000,
   });
 
@@ -37,40 +36,13 @@ export default function ISSTracker() {
 
   console.log(data);
 
-  // const [data, setCoords] = useState({
-  //   longitude: 0,
-  //   latitude: 0,
-  // });
-
-  // async function getISSCoords() {
-  //   try {
-  //     const response = await fetch(URL);
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     getISSCoords();
-  //   }, 5000);
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
-
   return (
     <main>
       <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
         longitude={data.longitude}
         latitude={data.latitude}
-        // onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
